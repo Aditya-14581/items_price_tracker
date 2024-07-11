43032a39d4d872affced2e30206d28615f43619a
Aditya Kumar <adityakumarsrj666@gmail.com>
2024-07-11 04:38:05 +0000

    product sent to whatsapp changed in hindi

--- pages/Home/jsobjects/sendItems/sendItems.js
+++ pages/Home/jsobjects/sendItems/sendItems.js
@@ -6,18 +6,25 @@ export default {
 			// copyToClipboard(keysString);
 	// },
 
-  sendProductListToWhatsApp() {
+ sendProductListToWhatsApp() {
     const keys = Object.keys(JSONForm3.formData);
-    const productList = keys.join('\n'); // Join keys with new line for better readability
+    
+    // Map through keys and extract text after the slash mark
+    const modifiedKeys = keys.map(key => {
+        const parts = key.split('/');
+        return parts[1] || parts[0]; // In case there is no slash, return the original key
+    });
+
+    const productList = modifiedKeys.join('\n'); // Join keys with new line for better readability
     
     // Encode the message for the WhatsApp URL
-    const message = encodeURIComponent(`Here is the product list:\n${productList}`);
+    const message = encodeURIComponent(`सब्जियों की सूची:\n${productList}`);
     
     // Create the WhatsApp URL with just the pre-filled message
     const whatsappURL = `https://wa.me/?text=${message}`;
     
     // Use navigateTo to open the WhatsApp URL
     navigateTo(whatsappURL);
-  },
+},
 
 };
